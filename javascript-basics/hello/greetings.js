920d19efe5aea1bc07303c5c7102d63e425ad11d
Tianxiao <tw1119@ic.ac.uk>
2020-08-13 23:07:55 +0800

    Functionalise the greetingBot

--- javascript-basics/hello/greetings.js
+++ javascript-basics/hello/greetings.js
@@ -3,15 +3,16 @@
 // it should return "Hello Bob!".
 // If no argument is passed, it shall return "Hello there!"
 
-const greetings = function () {
+const greeting = function (name) {
     /**This is function description */
     // you can write your codes here
-    return "This should return the name";
+    return "Replace this with your own codes";
+    // return "Hello, " + name + "!";
 };
 
 // we then export this as a module so codes outside this
 // JS file can import it can use the greetings function
-export default Object.freeze(greetings);
+export default Object.freeze(greeting);
 
  /*
  Top Tips: String Concatenation
--- javascript-basics/hello/hello.js
+++ javascript-basics/hello/hello.js
@@ -4,3 +4,10 @@
 console.log("Replace this with your own code");
 
 // it shall print "Hello World" (without the quatation marks)
+
+// now we will make a function that returns "Hello World!" when called
+
+const hello = () =>{
+    return "Replace this with your own code";
+};
+export Object.freeze(hello);
