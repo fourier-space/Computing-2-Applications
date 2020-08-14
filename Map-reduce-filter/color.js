8d5e135b1196ab9f359b75ef19366c048a244fca
pc619 <pc619@ic.ac.uk>
2020-08-14 12:31:35 +0400

    Start

---
+++ Map-reduce-filter/color.js
@@ -0,0 +1,15 @@
+const Color = Object.create(null);
+
+Color.noRed = function (...arrayRGB) {
+
+};
+
+Color.rgbToHexColor = function (arrayRGB) {
+
+};
+
+Color.mixColors = function (arrayRGB1, arrayRGB2) {
+
+};
+
+export default Object.freeze(Color);
---
+++ Map-reduce-filter/matrix.js
@@ -0,0 +1,11 @@
+const Matrix = Object.create(null);
+
+Matrix.singleDigitFilter = function (matrix) {
+
+};
+
+Matrix.findDeterminant = function (matrix) {
+
+};
+
+export default Object.freeze(Matrix);
