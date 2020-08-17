037fd73593e6652c6de9670fb3b3c1f582805d42
pc619 <pc619@ic.ac.uk>
2020-08-17 18:30:08 +0400

    oh

--- Map-reduce-filter/node_modules/fast-check/lib/esm/check/property/UnbiasedProperty.js
+++
@@ -1,8 +0,0 @@
-export class UnbiasedProperty {
-    constructor(property) {
-        this.property = property;
-        this.isAsync = () => this.property.isAsync();
-        this.generate = (mrng, _runId) => this.property.generate(mrng);
-        this.run = (v) => this.property.run(v);
-    }
-}
