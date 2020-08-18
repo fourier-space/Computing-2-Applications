046d11daa7a26fff17560015cce11b50b1e5501a
Tianxiao <tw1119@ic.ac.uk>
2020-08-18 11:48:23 +0800

    Updated node package.

--- luckydraw/luckyDraw-v2/randNumGen.js
+++ luckydraw/luckyDraw-v2/randNumGen.js
@@ -1,7 +1,7 @@
 // This module export a function that takes two boundaries
 // and returns a random integer between max and min
 
-const generateRandom = function (max, min) {
+const generateRandom = function (min, max) {
     const random = Math.floor(Math.random() * (max - min + 1) + min);
     // if max is 9 , min is 0, results can be [0,9]
     return random;
