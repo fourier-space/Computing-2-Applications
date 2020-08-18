e03c4dbc45623f6990bb9aec68ea8d636d1ba631
Tianxiao <tw1119@ic.ac.uk>
2020-08-18 11:41:40 +0800

    Added comments

--- closure-and-pure-functions/checkAge.js
+++ closure-and-pure-functions/checkAge.js
@@ -1,3 +1,10 @@
+// this is an example demonstrating that a variable declared by const
+// is not really a `constant`. This is to prevent re-assignment but
+// it can be updated.
+// Therefore, to eliminate side effects, writing pure functions
+// is always a good practice to be kept in mind.
+
+
 const legalDrinkingAge = {UK: 18, US: 21, China: 18, Greece: 17, Iceland: 20};
 const checkAge1 = function (country, age) {
     return age >= legalDrinkingAge[country];
