dd4fd747d6fb2add952e4a873e2db7b1cfde1962
Freddie Page <freddie.page@imperial.ac.uk>
2021-05-04 14:00:47 +0100

    today's class

---
+++ Week 2 - Javascript/web-app/server.js
@@ -0,0 +1,18 @@
+const recipient = "Freddie";
+const salutation = "Hello ";
+
+// Write a single line here.
+/*
+multiline
+is anything
+between
+these symbols.
+*/
+let greeting;
+if (recipient === "Trump") {
+    greeting = "Not you";
+} else {
+    greeting = salutation + recipient;
+}
+
+console.log(greeting);
