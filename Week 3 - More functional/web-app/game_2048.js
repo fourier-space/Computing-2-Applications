25dfbd70f028739ed2c0dfdcc8eb73c4ed00a916
Freddie Page <freddie.page@imperial.ac.uk>
2021-05-09 18:24:12 +0100

    identity

--- Week 3 - More functional/web-app/game_2048.js
+++ Week 3 - More functional/web-app/game_2048.js
@@ -1,8 +1,11 @@
 const game_2048 = Object.create(null);
 
-game_2048.left = (x) => x; // This is the identity function – does nothing.
-game_2048.right = (x) => x;
-game_2048.up = (x) => x;
-game_2048.down = (x) => x;
+// The identity function returns whatever it is passed. i.e does nothing.
+const identity = (x) => x;
+
+game_2048.left = identity; // this is a placeholder for a correct implementation
+game_2048.right = identity;
+game_2048.up = identity;
+game_2048.down = identity;
 
 export default Object.freeze(game_2048);
