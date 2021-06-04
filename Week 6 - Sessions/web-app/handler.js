1678d5373ecde5a52f866fcf2911aee9a75dcad1
Freddie Page <freddie.page@imperial.ac.uk>
2021-06-04 15:35:48 +0100

    session code

--- Week 6 - Sessions/web-app/handler.js
+++ Week 6 - Sessions/web-app/handler.js
@@ -1,9 +1,58 @@
+import session from "express-session";
+
 const Handler = Object.create(null);
 
-Handler.identify = function (request_object) {
-    return Promise.resolve({
-        "response": `Hi! ${request_object.name}`
-    });
+const contacts = [];
+const messages = [];
+
+Handler.identify = function (request_object, session_data) {
+    const name = request_object.name;
+
+    session_data.name = name;
+
+    contacts.push(name);
+
+    return Promise.resolve(
+        {
+            "response": `Identified as ${name}`
+        }
+    );
+};
+
+Handler.whoAmI = function (ignore, session_data) {
+    return Promise.resolve({"name": session_data.name});
+};
+
+Handler.listContacts = function () {
+    return Promise.resolve(contacts);
+};
+
+Handler.sendMessage = function (request_object, session_data) {
+    const to = request_object.to;
+    const body = request_object.body;
+    const message = {
+        "to": to,
+        "from": session_data.name,
+        "body": body
+    };
+    messages.push(message);
+    return Promise.resolve({"response": "Message sent"});
+};
+
+Handler.listOutbox = function (ignore, session_data) {
+    return Promise.resolve(
+        messages.filter(
+            (message) => message.from === session_data.name
+        )
+    );
+};
+
+Handler.listInbox = function (ignore, session_data) {
+    return Promise.resolve(
+        messages.filter(
+            (message) => message.to === session_data.name
+        )
+    );
 };
 
 export default Object.freeze(Handler);
