7991649b364a70ec87975bab189c88f203232735
Freddie Page <freddie.page@imperial.ac.uk>
2022-05-02 19:02:33 +0100

    Remove previous code samples—accessible in branch

--- Week 1 - Template for web apps/web-app/reverse.js
+++
@@ -1,8 +0,0 @@
-const Reverse = Object.create(null);
-
-// Takes a string and reverses the order of its characters.
-Reverse.reverse = function (string) {
-    return string.split("").reverse().join("");
-};
-
-export default Object.freeze(Reverse);
